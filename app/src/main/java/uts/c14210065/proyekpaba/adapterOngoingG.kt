529023ed7cbeda75792da0327d809ff401620613
c14210065 <c14210065@john.petra.ac.id>
2023-12-21 23:37:43 +0700

    ganti gym
    gym buat yang udah booked -> tombol enable == false (masih belum bisa). On going gym yang bisa dicancel cuma yang lebih dari 24 jam.

--- app/src/main/java/uts/c14210065/proyekpaba/adapterOngoingG.kt
+++ app/src/main/java/uts/c14210065/proyekpaba/adapterOngoingG.kt
@@ -14,11 +14,14 @@ import com.google.firebase.Timestamp
 import com.google.firebase.firestore.FieldValue
 import com.google.firebase.firestore.firestore
 import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Date
 import java.util.Locale
+import java.util.TimeZone
 
 class adapterOngoingG(
     private val listOGym: ArrayList<Gym>,
-    private val idLogin: String?
+    private val idLogin: String?,
 ) : RecyclerView.Adapter<adapterOngoingG.ListViewHolder>() {
 
     private lateinit var onItemClickCallback: OnItemClickCallback
@@ -51,18 +54,32 @@ class adapterOngoingG(
         var gym = listOGym[position]
 
         holder._tvSession.text = gym.sesi
-        holder._btnCancelGym.setOnClickListener{
-            onItemClickCallback.delData(position)
-        }
 
+        val db = Firebase.firestore
+
+        holder._btnCancelGym.setOnClickListener {
+            // BLM DIBERI IF KALAU CUMA BISA  LEBIH DARI 24 JAM
+
+            // Get the timestamp for the formattedDate and sesi
+            Log.d("TimeComparison", "gym tanggal : ${gym.tanggal} ")
+            Log.d("TimeComparison", "gym sesi : ${gym.sesi} ")
+            val timestamp = convertDateTimeToTimestamp(gym.tanggal, gym.sesi)
+            Log.d("TimeComparison", "timestamp : ${timestamp} ")
+            if (isMoreThan24HoursBefore(timestamp)) {
+                onItemClickCallback.delData(position)
+            } else {
+                showAlert(holder.itemView.context, "Pembatalan Gagal", "Booking sesi gym tidak bisa dibatalkan karena sudah lebih dari 24 jam.")
+            }
+
+        }
     }
+
 
     fun showAlert(context: Context, title: String, message: String) {
         val builder = AlertDialog.Builder(context)
         builder.setTitle(title)
         builder.setMessage(message)
         builder.setPositiveButton("OK") { dialog, which ->
-            // Handle the "OK" button click if needed
             dialog.dismiss()
         }
         val dialog: AlertDialog = builder.create()
@@ -86,6 +103,34 @@ class adapterOngoingG(
         }
         return Timestamp.now() // Return a default timestamp in case of an error
     }
+
+
+    //sek blm paham cara kerja
+    fun isMoreThan24HoursBefore(tanggal: Timestamp?): Boolean {
+        if (tanggal != null) {
+            Log.d("TimeComparison", "tanggal : $tanggal")
+            val timeZone = TimeZone.getTimeZone("Asia/Jakarta") // Set the time zone to Indonesia
+
+            val currentTimeInTimeZone = Calendar.getInstance(timeZone).timeInMillis
+            val twentyFourHoursInMillis = 24 * 60 * 60 * 1000
+
+            // Set the time zone for the target time
+            val targetTimeInTimeZone = Calendar.getInstance(timeZone).apply {
+                time = tanggal.toDate() // Convert the Timestamp to Date
+            }.timeInMillis
+
+            val twentyFourHoursBefore = targetTimeInTimeZone - twentyFourHoursInMillis // Corrected calculation
+
+            Log.d("TimeComparison", "currentTime: $currentTimeInTimeZone")
+            Log.d("TimeComparison", "twentyFourHoursBefore: $twentyFourHoursBefore")
+
+            return currentTimeInTimeZone < twentyFourHoursBefore
+        }
+        return false
+    }
+
+
+
 
 
 }
