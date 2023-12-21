44104a24149b65a35cd51f9a952ba57fc816370f
c14210065 <c14210065@john.petra.ac.id>
2023-12-21 14:42:51 +0700

    ui update + ongoing

---
+++ app/src/main/java/uts/c14210065/proyekpaba/OnGoing.kt
@@ -0,0 +1,8 @@
+package uts.c14210065.proyekpaba
+
+data class OnGoing(
+    var id: String,
+    var tanggal: String,
+    var sesi:String,
+    var userTrainerId: String
+)
--- app/src/main/java/uts/c14210065/proyekpaba/activityOngoing.kt
+++ app/src/main/java/uts/c14210065/proyekpaba/activityOngoing.kt
@@ -1,7 +1,27 @@
 package uts.c14210065.proyekpaba
 
+import android.annotation.SuppressLint
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.AbsoluteSizeSpan
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.Firebase
+import com.google.firebase.Timestamp
+import com.google.firebase.firestore.FieldValue
+import com.google.firebase.firestore.firestore
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Date
+import java.util.Locale
+import java.util.TimeZone
 
 class activityOngoing : AppCompatActivity() {
     companion object {
@@ -9,8 +29,183 @@ class activityOngoing : AppCompatActivity() {
         const val userId = "GETID"
     }
 
+    private var arOngoingG = arrayListOf<Gym>()
+    private var lastClickedButton: Button? = null
+    lateinit var dayDate: Date
+    var idLogin: String = ""
+    private lateinit var buttons: Array<Button>
+    lateinit var idTrainer : String
+    lateinit var _rvOngoing : RecyclerView
+    lateinit var userTrainerId : String
+    var state : Int = 0
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_ongoing)
+        setContentView(R.layout.activity_sesi_trainer)
+
+        idLogin = intent.getStringExtra(utama.userId).toString()
+        Log.d("trainer", idLogin)
+
+        _rvOngoing = findViewById<RecyclerView>(R.id.rvOngoing)
+        val _btnOGym = findViewById<Button>(R.id.btnOGym)
+        val _btnOTrainer = findViewById<Button>(R.id.btnOTrainer)
+        val _btnOClass = findViewById<Button>(R.id.btnOClass)
+
+        val _btnd1 = findViewById<Button>(R.id.btnd1)
+        val _btnd2 = findViewById<Button>(R.id.btnd2)
+        val _btnd3 = findViewById<Button>(R.id.btnd3)
+        val _btnd4 = findViewById<Button>(R.id.btnd4)
+        val _btnd5 = findViewById<Button>(R.id.btnd5)
+        val _btnd6 = findViewById<Button>(R.id.btnd6)
+        val _btnd7 = findViewById<Button>(R.id.btnd7)
+
+        // Get the current date
+        var currentDate = Calendar.getInstance().time
+        dayDate = currentDate
+
+        buttons = arrayOf(_btnd1, _btnd2, _btnd3, _btnd4, _btnd5, _btnd6, _btnd7)
+
+        _btnOGym.setOnClickListener{
+            state = 1
+        }
+
+        _btnOTrainer.setOnClickListener{
+            state = 2
+        }
+
+        _btnOClass.setOnClickListener{
+            state = 3
+        }
+
+        for (i in buttons.indices) {
+            val day = Calendar.getInstance()
+            day.time = currentDate
+            day.add(Calendar.DATE, i)
+
+            val formattedDate = SimpleDateFormat("EEE\ndd MMM", Locale("id", "ID")).format(day.time)
+
+            dayTextSet(buttons[i], formattedDate, fGym.size1, fGym.size2)
+            buttons[i].visibility = View.GONE
+            buttons[i].setOnClickListener {
+                // Reset the background color of the last clicked button to white
+                lastClickedButton?.setBackgroundColor(Color.WHITE)
+
+                // Set the background color of the current clicked button to purple
+                buttons[i].setBackgroundColor(Color.parseColor("#C9F24D"))
+
+                // Update the last clicked button
+                lastClickedButton = buttons[i]
+
+                // tanggal di button
+                val calendar = Calendar.getInstance()
+                calendar.time = currentDate
+                calendar.add(Calendar.DATE, i)
+                dayDate = calendar.time
+                if(state == 1) {
+                    TampilkanDataGym(dayDate)
+                }
+            }
+        }
+//        _rvOngoing.layoutManager = LinearLayoutManager(this)
+//        val adapterP = adapterSesiT(arOngoingG, idLogin)
+//        _rvOngoing.adapter = adapterP
+    }
+
+    fun dayTextSet(button: Button, text: String, size1: Int, size2: Int) {
+        val spannableString = SpannableString(text)
+
+        spannableString.setSpan(
+            AbsoluteSizeSpan(size1, true),
+            0,
+            text.indexOf('\n'),
+            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+        spannableString.setSpan(
+            AbsoluteSizeSpan(size2, true),
+            text.indexOf('\n') + 1,
+            spannableString.length,
+            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+
+        button.text = spannableString
+
+        button.setOnClickListener {
+            button.isEnabled = false
+        }
+    }
+
+    val db = Firebase.firestore
+
+    //menampilkan data gy,
+    private fun TampilkanDataGym(btnDate: Date) {
+        db.collection("GymSesi").get().addOnSuccessListener { result ->
+            arOngoingG.clear()
+            for (document in result) {
+                var Gymid = document.id
+                val tanggal = (document["tanggal"] as? Timestamp)?.toDate()
+                val kuotaSisa = document.getLong("kuotaSisa")?.toInt() ?: 0
+
+                if(cekDate(tanggal, btnDate)) {
+                    val userId =
+                        (document["userId"] as? List<*>)?.map { it.toString() } ?: emptyList()
+
+                    if (idLogin in userId) {
+                        val kuotaMax = document.getLong("kuotaMax")?.toInt() ?: 0
+
+                        val calendar = Calendar.getInstance()
+                        calendar.time = tanggal
+
+                        // ambil jam dan menit dari timestamp tanggal di database
+                        val jam = calendar.get(Calendar.HOUR_OF_DAY)
+                        val menit = calendar.get(Calendar.MINUTE)
+
+                        //agar jadi 08.00 atau 17.00
+                        val formatJam = String.format("%02d", jam)
+                        val formatMenit = String.format("%02d", menit)
+
+                        val sesi = "$formatJam:$formatMenit"
+
+                        val member = document.getBoolean("member") ?: false
+
+                        val dateFormat = SimpleDateFormat("dd MMM yyyy", Locale.ENGLISH)
+                        dateFormat.timeZone = TimeZone.getTimeZone("Asia/Jakarta")
+                        val formattedDate = dateFormat.format(tanggal)
+                        Log.d("formatted tanggal : ", formattedDate)
+
+                        arOngoingG.add(
+                            Gym(
+                                Gymid,
+                                formattedDate,
+                                sesi,
+                                kuotaMax,
+                                kuotaSisa,
+                                ArrayList(userId),
+                                member
+                            )
+                        )
+                        Log.d("haes", "Document data: ${document.data}")
+                        Log.d("haes", formattedDate)
+                    }
+                }
+            }
+            arOngoingG.sortBy { it.sesi }
+            _rvOngoing.adapter?.notifyDataSetChanged()
+        }.addOnFailureListener { e ->
+            Log.e("haes", "Error fetching data from Firebase", e)
+        }
+    }
+
+    fun cekDate(timestamp: Date?, btnDate: Date): Boolean {
+        val dateFormat = SimpleDateFormat("dd MMM", Locale("id", "ID"))
+        dateFormat.timeZone = TimeZone.getTimeZone("Asia/Jakarta")
+
+        val formattedDate = dateFormat.format(timestamp)
+        val date = dateFormat.format(btnDate)
+
+        Log.d("cektanggal", "format" + formattedDate)
+        Log.d("cektanggal", "button " + date)
+
+        return formattedDate == date
     }
 }
