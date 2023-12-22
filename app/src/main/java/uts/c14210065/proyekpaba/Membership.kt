9169708004f7e67d5feddcd3454b1e8dd79b1dfb
cathNotcat <catherine.rosalind21@gmail.com>
2023-12-22 23:13:52 +0700

    membership page

---
+++ app/src/main/java/uts/c14210065/proyekpaba/Membership.kt
@@ -0,0 +1,11 @@
+package uts.c14210065.proyekpaba
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Membership : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_membership)
+    }
+}
