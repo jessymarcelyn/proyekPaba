a66c70bfaaa9ff984d1d2dbb5a3da693f1c266af
cathNotcat <catherine.rosalind21@gmail.com>
2023-12-27 14:17:21 +0700

    perbaikan no 2

--- app/src/main/java/uts/c14210065/proyekpaba/inputJadwalTrainer.kt
+++ app/src/main/java/uts/c14210065/proyekpaba/inputJadwalTrainer.kt
@@ -5,22 +5,28 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Button
 import android.widget.CalendarView
 import android.widget.ImageView
 import android.widget.RadioButton
 import android.widget.RadioGroup
+import android.widget.TextView
+import android.widget.Toast
 import com.google.firebase.Firebase
+import com.google.firebase.Timestamp
 import com.google.firebase.firestore.firestore
 import java.text.SimpleDateFormat
 import java.util.Calendar
 import java.util.Locale
 
 class inputJadwalTrainer : AppCompatActivity() {
-    lateinit var dateSelect: String
+    var dateSelect: String = ""
     lateinit var jamSelect: String
     lateinit var btnSave: Button
     lateinit var btnBack: ImageView
+    lateinit var warningTgl: TextView
+    lateinit var warningJam: TextView
 
     var idLogin: String = ""
     var db = Firebase.firestore
@@ -34,6 +40,11 @@ class inputJadwalTrainer : AppCompatActivity() {
         val radioGroup = findViewById<RadioGroup>(R.id.radioGroupJam)
         btnSave = findViewById(R.id.btnSaveDateJadwal)
         btnBack = findViewById(R.id.btnInputBack)
+
+        warningTgl = findViewById(R.id.warningTgl)
+        warningJam = findViewById(R.id.warningJam)
+
+        jamSelect = ""
 
         btnBack.setOnClickListener {
             val intent = Intent(this, activityTrainer::class.java)
@@ -53,48 +64,77 @@ class inputJadwalTrainer : AppCompatActivity() {
         }
 
         radioGroup.setOnCheckedChangeListener { _, checkedId ->
-            val radioButton = findViewById<RadioButton>(checkedId)
-            jamSelect = radioButton.text.toString()
+                when (checkedId) {
+                    R.id.jam1 -> jamSelect = "08:00:00"
+                    R.id.jam2 -> jamSelect = "10:00:00"
+                    R.id.jam3 -> jamSelect = "14:00:00"
+                    R.id.jam4 -> jamSelect = "16:00:00"
+                }
+                Log.d("inputJam", "jamselect: $jamSelect")
         }
+        Log.d("inputJam", "jamselect: $jamSelect")
 
         btnSave.setOnClickListener {
-            SimpanTanggalJam(dateSelect, jamSelect)
+            dateSelect?.let { date ->
+                SimpanTanggalJam(date, jamSelect)
+            }
         }
     }
 
     private fun SimpanTanggalJam(tgl: String, jam: String) {
+
+        val selectedCalendar = Calendar.getInstance()
+        if (tgl.isNotEmpty() && jam.isNotEmpty()) {
+            selectedCalendar.time = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault()).parse("$tgl $jam")!!
+
+            val currentDate = Calendar.getInstance().time
+            if (selectedCalendar.time < currentDate) {
+                Toast.makeText(this, "Tanggal dan jam harus lebih dari sekarang.", Toast.LENGTH_SHORT).show()
+                Log.e("inputJadwal", "Tanggal harus lebih dari hari ini. tgl: $tgl jam: $jam")
+                return
+            }
+        } else {
+            if (tgl.isEmpty()) {
+                warningTgl.visibility = View.VISIBLE
+            }
+            else if (jam.isEmpty()) {
+                warningJam.visibility = View.VISIBLE
+            }
+            Log.e("inputJadwal", "tgl or jam is empty. tgl: $tgl jam: $jam")
+            return
+        }
+
+        val timestamp = Timestamp(selectedCalendar.time)
+
         db.collection("JadwalTrainer").get().addOnSuccessListener { result ->
             var docId: String? = null
 
-            val addData = hashMapOf(
-                "trainerId" to idLogin,
-                "tanggal" to tgl,
-                "jam" to jam
-            )
-
-            val setData = hashMapOf(
-                "tanggal" to tgl,
-                "jam" to jam
-            )
-
             for (document in result) {
                 var trainerId = document.getString("trainerId")
+                var timestmp = document.getTimestamp("tanggal")
 
-                if (trainerId == idLogin) {
+                if (trainerId == idLogin && timestmp == timestamp) {
                     docId = document.id
                     break
                 }
             }
-//            if (docId == null) {
+
+            var utid = ""
+            val addData = hashMapOf(
+                "trainerId" to idLogin,
+                "tanggal" to timestamp,
+                "userTrainerId" to utid
+            )
+            if (docId == null) {
                 db.collection("JadwalTrainer").add(addData).addOnSuccessListener {
+                    Toast.makeText(this, "Jadwal berhasil ditambahkan", Toast.LENGTH_SHORT).show()
                     Log.d("inputJadwal", "berhasil add data.")
                 }
-//            }
-//            else {
-//                db.collection("UserTrainer").document().set(setData).addOnSuccessListener {
-//                    Log.d("inputJadwal", "berhasil set data. docid: $docId")
-//                }
-//            }
+            }
+            else {
+                Toast.makeText(this, "Jadwal sudah ada", Toast.LENGTH_SHORT).show()
+                Log.d("inputJadwal", "jadwal sdh ada. docid: $docId")
+            }
         }
     }
 }
